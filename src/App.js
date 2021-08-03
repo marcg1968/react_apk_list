cad8c5f8e17f24f5274f8ad2a2055f02f193b8d7
Marc Greyling <marc@cubi201.marcjan.de>
2021-08-03 11:22:29 +0200

    improvements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 // App.js
 
 import './App.css'
-import {useEffect, useRef, useState} from "react"
+import {useCallback, useEffect, useRef, useState} from "react"
 import Listing from "./Listing";
 // const { REACT_APP_URL } = process.env
 
 const URL = process.env.NODE_ENV === 'development'
     ? 'https://marcg.ddnss.org/apk/index.cgi?listing'
     : './index.cgi?listing'
-// const ELAPSED = 1000 * 60 * 5
+// const INTERVAL = 1.5 * 1000 * 60 /* update interval in msecs, e.g. every 1.5 minutes */
+const INTERVAL = 9 * 1000
 
 const App = () => {
 
@@ -19,47 +20,74 @@ const App = () => {
     const mountedRef = useRef(false)
     const firstAccessRef = useRef(+new Date())
 
-    const doFetch = () => {
+    // const tellNewLoc = useCallback((i, {x, y}, stack, posReached) => {
+    const doFetch = useCallback(() => {
         if (!mountedRef.current) return
         fetch(URL)
             .then(r => r.json())
             .then(data => {
+                // console.log(29, data)
                 setLastFetch(+new Date())
-                console.log(20, data, typeof data)
                 data.sort((a, b) => a.ts < b.ts ? 1 : -1)
                 if (mountedRef.current) {
                     setFiles(data)
                 }
-                // highlight if updated since first access
+                // /* highlight if updated since first access */
+                // /* NOTE: this will continually update the newSince state var and thus cause a re-render */
+                // if (firstAccessRef && data) {
+                //     let _newSince = data.filter(({ts}) => ts * 1000 > firstAccessRef.current)
+                //     if (_newSince.length) {
+                //         setNewSince(_newSince)
+                //     }
+                // }
+
+                /* only update newSince arr if ts changed */
                 if (firstAccessRef && data) {
-                    // && firstAccessRef.current + ELAPSED > +new Date()
-                    // const arr = data.filter(({ts}) => ts * 1000 > firstAccessRef.current)
-                    // console.log(firstAccessRef.current, {arr})
-                    setNewSince(data.filter(({ts}) => ts * 1000 > firstAccessRef.current))
+                    let _newSince = data.filter(({ts}) => ts * 1000 > firstAccessRef.current)
+                    if (_newSince.length && newSince.length === 0) {
+                        return setNewSince(_newSince)
+                    }
+                    _newSince.map(({ts, file}) => {
+                        return setNewSince(prev => {
+                            let i
+                            /* if exact match skip out without updating */
+                            i = prev.findIndex(e => e.ts === ts && e.file === file)
+                            // console.log(62, {i})
+                            if (i >= 0) return prev
+                            /* if time diff - update existing */
+                            i = prev.findIndex(e => e.ts !== ts && e.file === file)
+                            // console.log(67, {i})
+                            if (i >= 0) {
+                                prev = prev.splice(i, 1) /* create new element to force reload */
+                                return [...prev, {ts, file}]
+                            }
+                            /* reached here the items is hitherto unknown and must be added */
+                            // console.log(73, {i})
+                            return prev.concat({ts, file})
+                        })
+                    })
                 }
             })
-    }
-
-    useEffect(() => {
-        console.log(44, newSince)
     }, [newSince])
 
     useEffect(() => {
-        // let mounted = true
-        // fetch(URL)
-        //     .then(r => r.json())
-        //     .then(data => {
-        //         console.log(20, data, typeof data)
-        //         data.sort((a, b) => a.ts < b.ts ? 1 : -1)
-        //         if (mounted) {
-        //             setFiles(data)
-        //         }
-        //     })
+        // console.log(44, newSince)
+        const plural = newSince.length > 1
+        const isAre = plural ? 'are' : 'is'
+        if (newSince.length) {
+            console.log(`There ${isAre} ${newSince.length} new or updated `
+                + `file${plural ? 's' : ''} since page was loaded:`)
+            console.table(newSince)
+        }
+
+    }, [newSince])
+
+    useEffect(() => {
         mountedRef.current = true
         doFetch()
         // return () => mounted = false
         return () => mountedRef.current = false
-    }, [])
+    }, [doFetch])
 
     const useWindowSize = () => { /* cf https://usehooks.com/useWindowSize/ */
         const [windowSize, setWindowSize] = useState({
@@ -103,11 +131,10 @@ const App = () => {
 
     useInterval(() => {
         doFetch()
-    }, 9000)
+    }, INTERVAL)
 
     const { width } = useWindowSize()
-    let direction = width > 1024 ? 'row' : 'column'
-    let cross = width <= 1024 ? 'column' : 'row'
+    let direction = width > 1024 ? 'row' : 'column' /* small devices: main direction column */
 
     return (
         <div
@@ -117,17 +144,14 @@ const App = () => {
                 minHeight: '99vh',
             }}
         >
+            {/*{(new Date(firstAccessRef.current)).toISOString()}*/}
+            {/*<pre>{JSON.stringify(newSince, null, 2)}</pre>*/}
             <header>
-                {/*<p>process.env.NODE_ENV = <code>{process.env.NODE_ENV}</code></p>*/}
-                {/*<p>URL = <code>{URL}</code></p>*/}
-                {/*<p>width = <code>{width}</code></p>*/}
-                {/*<p>direction = <code>{direction}</code></p>*/}
                 <h1><code>.apk</code> Files for Download</h1>
             </header>
 
             <Listing
                 direction={direction}
-                cross={cross}
                 files={files}
                 newSince={newSince}
             />
--- src/Listing.js
+++ src/Listing.js
@@ -4,123 +4,72 @@ const Listing = props => {
 
     const {
         direction,
-        cross,
         files,
         newSince,
     } = props
 
+    const styleFlexMain = direction === 'column' ? { flexDirection: 'column'} : {}
+
     return (
-        <section>
-            <div
-                style={{
-                    display: 'flex', flexDirection: direction, alignItems: 'center',
-                    // backgroundColor: 'rgba(128,63,63,0.66)',
-                    paddingBottom: '3rem',
-                    // marginBottom: '3rem',
-                }}>
-                <div
-                    style={{
-                        flex: 1,
-                        // border: '10px solid green',
-                    }}>&nbsp;</div>
-                <div
-                    style={{
-                        flex: 6,
-                        border: '10px solid rgba(128,63,63,0.66)',
-                        flexDirection: cross,
-                        backgroundColor: '#fefefe',
-                        paddingBottom: '3rem',
-                    }}>
+        <section className={'listing'} style={{...styleFlexMain}}>
+            <div className={'colSpacer'}/>
+            <div className={'listingContainer'} style={{...styleFlexMain}}>
 
-                    {files.length && files.map(({file, ts}, i, self) => {
+                {files.length && files.map(({file, ts}, i, self) => {
 
-                        const foundNewSince = newSince.find(e => e.file === file)
-                        const highlight = foundNewSince ? foundNewSince.file === file : false
+                    const foundNewSince = newSince.find(e => e.file === file)
+                    const highlight = foundNewSince ? foundNewSince.file === file : false
 
-                        let d = new Date(ts * 1000)
-                            .toISOString()
-                            .substr(0, 16)
-                            // .replace('T', ' @ ')
-                            .replace('T', ' ')
-                            .replace(':', 'h')
-                        let dHighlight = highlight
-                            ? 'New since last access:'
-                            : null
-                        let title = file.replace(/\.apk$/i, '')
-                        // direction = 'column' // force
+                    let d = new Date(ts * 1000)
+                        .toISOString()
+                        .substr(0, 16)
+                        // .replace('T', ' @ ')
+                        .replace('T', ' ')
+                        .replace(':', 'h')
+                    let dHighlight = highlight
+                        ? 'New since last access:'
+                        : null
+                    let title = file.replace(/\.apk$/i, '')
 
-                        return (
-                            <div
-                                key={'item-' + i}
-                                style={{
-                                    flex: 1,
-                                    // border: '5px solid yellow'
-                                }}
-                            >
-                                {i === 0 ? <h3>Most recent:</h3> : null}
+                    return (
+                        <div className={'rowOuter'} key={'item-' + i}>
+                            {i === 0 ? <h3>Most recent:</h3> : null}
 
-                                <div
-                                    style={{
-                                        display: 'flex', flexDirection: direction, alignItems: 'center'
-                                    }}
-                                >
-                                    <div style={{ flex: 2, textAlign: 'right', }}
-                                    >
-                                        {dHighlight}
-                                        {dHighlight && <br/>}
-                                        {d}
-                                    </div>
-                                    <h2 style={{ flex: 3 }}>{title}</h2>
-                                    <h3
-                                        style={{ flex: 2, textAlign: 'left', }}
-                                    >
-                                        <a
-                                            href={'./' + file}
-                                            className={highlight ? 'hottest hottestL' : ''}
-                                        >Download</a>
-                                        <span
-                                            className={highlight ? 'hottest hottestR' : ''}
-                                        >
-                                            {highlight ? ' <<< new!' : null}
-                                        </span>
-                                    </h3>
+                            <div className={'rowInner'} style={{...styleFlexMain}}>
+                                <div className={'col1'}>
+                                    {dHighlight}
+                                    {dHighlight && <br/>}
+                                    {d}
                                 </div>
-
-                                {i === 0
-                                    ? (
-                                        <div
-                                            style={{
-                                                display: 'flex', flexDirection: direction,
-                                                alignItems: 'center',
-                                            }}
-                                        >
-                                            <div style={{ flex: 1 }}>&nbsp;</div>
-                                            <div
-                                                style={{
-                                                    flex: 3,
-                                                    // border: '5px solid crimson',
-                                                    borderTop: '1px solid #282c34',
-                                                    paddingTop: '0.67rem',
-                                                    marginTop: '2rem',
-                                                }}
-                                            >
-                                                <h4
-                                                    style={{
-                                                        // borderTop: '1px solid #282c34'
-                                                    }}
-                                                >Older releases:</h4>
-                                            </div>
-                                            <div style={{ flex: 1 }}>&nbsp;</div>
-                                        </div>
-                                    )
-                                    : null
-                                }
+                                <h2 className={'col2'}>{title}</h2>
+                                <h3 className={'col3'}>
+                                    <a
+                                        href={'./' + file}
+                                        className={highlight ? 'hottest hottestL' : ''}
+                                    >Download</a>
+                                    <span className={highlight ? 'hottest hottestR' : ''}>
+                                        {highlight ? ' <<< new!' : null}
+                                    </span>
+                                </h3>
                             </div>
-                        )
-                    })}
-                </div>
-                <div style={{ flex: 1 }}>&nbsp;</div>
+
+                            {i === 0
+                                ? (
+                                    <div className={'rowOlderOuter'}>
+                                        <div className={'colSpacer'}/>
+                                        <div className={'rowOlderInner'}>
+                                            <h4>Older releases:</h4>
+                                        </div>
+                                        <div className={'colSpacer'}/>
+                                    </div>
+                                )
+                                : null
+                            }
+                        </div>
+                    )
+                })}
             </div>
+            <div className={'colSpacer'}/>
         </section>
     )
 }
